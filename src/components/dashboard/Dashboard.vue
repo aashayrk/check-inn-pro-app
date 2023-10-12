73ece94efd26a41a2e5bba919aead1713181a446
aashayrk <aashayrk@gmail.com>
2023-10-12 09:05:44 +0530

    Finalized dashboard.

--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -2,6 +2,16 @@
   <div>
     <page page-title="Dashboard" tab-bar="true">
       <page-topbar page-title="Dashboard" menu="true"></page-topbar>
+
+      <!-- temp login -->
+      <div class="flex justify-end mb-2">
+        <router-link to="/login">
+          <button class="btn btn-transparent">
+            <span>Login</span>
+            <msr-icon>arrow_forward</msr-icon>
+          </button>
+        </router-link>
+      </div>
       <occupancy-overview></occupancy-overview>
       <transactions-overview class="mt-2"></transactions-overview>
       <collection-overview class="mt-2"></collection-overview>
